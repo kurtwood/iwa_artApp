bfcf105b57c0b028c10f6185a157945f825246a2
Kinterror <g.dupont23@gmail.com>
2014-03-21 11:14:59 +0100

    update website. add output field in index.html. Modify the design.

--- src/static/js/artApp.js
+++ src/static/js/artApp.js
@@ -42,4 +42,24 @@ $('#submitNoidea').on('click', function(e){
     });
 });
 
+function get_location() {
+  if (Modernizr.geolocation) {
+    navigator.geolocation.getCurrentPosition(show_map, handle_error);
+  } else {
+        handle_error();
+  }
+}
+
+var longitude;
+var latitude;
+function show_map(position) {
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+}
+
+function handle_error(err) {
+  if (err.code == 1) {
+    window.alert("Uups, something went wrong!");
+  }
+}
 
